2b34dd6b188bfd3226a07c3d056e7545c3c3c18b
ChristopheLagaillarde <chrislag94@gmail.com>
2022-05-24 15:39:21 +0400

    update of the main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from square_of import square_of
 
 
 def main():
-    print(square_of(int(input("Saisir un nombre:"))))
+    print(square_of(int(input("Input a number, we will tell it's square: "))))
 
 
 if __name__ == "__main__":
